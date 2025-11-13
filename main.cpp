1a650e36e6f227d8a16991955adbeb9646b9a5cd
Ian Torres <iantorres@outlook.com>
2025-11-13 17:44:39 -0300

    Fix Clang.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
+#include <framework/router.hpp>
 #include <framework/server.hpp>
 #include <framework/state.hpp>
-#include <framework/router.hpp>
 
 using namespace framework;
 
 int main() {
-    const auto _server = std::make_shared<server>();
-    shared_router _router = _server->get_state()->get_router();
-    _server->start(9000);
-    return 0;
+  const auto _server = std::make_shared<server>();
+  shared_router _router = _server->get_state()->get_router();
+  _server->start(9000);
+  return 0;
 }
